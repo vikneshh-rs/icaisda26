502f564a2436c6ce3cb88abd9768e3f212c5d223
Viknesh RS <vikneshhrs@gmail.com>
2026-09-24 00:43:31 +0530

    Update Proceedings Author Guidelines to direct Google Drive viewer URL

--- CSE-Conference-main/src/Components/Home/AnnouncementSlider.jsx
+++ CSE-Conference-main/src/Components/Home/AnnouncementSlider.jsx
@@ -15,7 +15,7 @@ const AnnouncementSlider = () => {
       badge: "OFFICIAL",
       title: "Proceedings Author Guidelines Published",
       desc: "Check the comprehensive proceedings author instructions & formatting slides.",
-      link: "https://docs.google.com/presentation/d/11jGtYTXokSTyP0UPXTRaVJ3o_fvYBbN7/edit?usp=sharing&ouid=114098769929117624762&rtpof=true&sd=true",
+      link: "https://drive.google.com/file/d/11jGtYTXokSTyP0UPXTRaVJ3o_fvYBbN7/view?usp=sharing",
       linkText: "View Author Guidelines",
       icon: <BookOpen className="w-4 h-4 text-emerald-400 shrink-0" />,
     },
